88ee7bb937fd3a1365d8deb8d04420d3b8bfed68
Iolandavasile <vasileiolanda81@yahoo.com>
2025-10-27 15:35:10 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NumerologyApp from "./components/NumerologyApp.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NumerologyApp />
+  </React.StrictMode>
+);
