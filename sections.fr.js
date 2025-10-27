6d48f5dfa646e87b48528dbe4d257ac20c884ef9
Iolandavasile <vasileiolanda81@yahoo.com>
2025-10-27 15:54:57 +0100

    Create sections.fr.js

---
+++ sections.fr.js
@@ -0,0 +1,11 @@
+export default {
+ 
+"Vibratia interioara": ` 
+Plusuri 1
+• cel care tinde să pătrundă (în interior);
+• cel care explorează în interiorul necunoscutului, dar caută să nu se împartă. Caută continuu să scoată ceva în lumină (ca soarele) așa că nu suportă lucrurile ascunse, tăinuite, întunecate, acoperite;
+• 1 - original, independent, indivizibil, constructiv, intelectual, logic, îndrăzneț, individual, constructiv, competitiv, curajos, câștigător, puternic, creativ, activ, pionier, leader care deschide calea, drept, hotărât și dârz; pasional, se împlinește prin a GÂNDI PENTRU a da, a realiza - se împlinește prin realizările proprii! Inventiv, mental puternic, plin de idei, Influencer, expansiv; egocentric; centrat pe a ajunge la potențialul său maxim; model demn de urmat, lider autosuficient, dornic să fie primul în tot și toate; “vede” îngust și adânc; focus; se poate concentra puternic într-o acțiune, dar pierde imaginea de ansamblu.
+• Vibrația pe care o reprezintă este masculină (energie centrifugă), “1” va dori de fiecare dată să fie primul în tot și în toate, să fie cel unic, acel om ce se aruncă în vâltoarea vieții, fiind cel ce deschide calea, a sa personală și a celor din jurul său. Este cel ce va dori să exploreze tot ce înseamnă nou, neexplorat, tot ce înseamnă necunoscut.
+Lecția descoperirii de sine! Să devină un conducător!  tu nu. `,
+};
+
